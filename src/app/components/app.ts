f5ff749bb4bbe083cde2ecc3b23b3fe476474c19
Jay Kan <leicasper@gmail.com>
2017-01-20 09:05:16 -0800

    chore: add app.ts

---
+++ src/app/components/app.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import './app.scss';
+
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  template:`  
+  <app-header></app-header>
+  <router-outlet></router-outlet> 
+  `
+})
+export class AppComponent {
+
+}
