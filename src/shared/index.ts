b4ee5a582bbb8bedf3f88af235a63bea4e080a28
Jay Kan <leicasper@gmail.com>
2017-01-23 10:54:51 -0800

    chore: add shared module

---
+++ src/shared/index.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+
+  ],
+  exports: [
+    CommonModule,
+  ]
+})
+export class SharedModule {}
