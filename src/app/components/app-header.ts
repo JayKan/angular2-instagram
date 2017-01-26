f23a5f59021441f991b382c6dc6a29f3648dbe6c
Jay Kan <leicasper@gmail.com>
2017-01-26 10:54:56 -0800

    feat(app-header): add app-header cmp

---
+++ src/app/components/app-header.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import './app-header.scss';
+
+@Component({
+  selector: 'app-header',
+  encapsulation: ViewEncapsulation.None,
+  template:`
+  <header class="header">
+    <div class="header-gradient"></div>
+    <div class="container">     
+      <a href="https://github.com/JayKan/angular2-instagram" target="_blank">
+        <span class="header-github">
+          <i class="fa fa-github" aria-hidden="true"></i>
+        </span>        
+      </a>     
+    </div>
+  </header>
+  `
+})
+export class AppHeaderComponent {
+
+}
