c437c3492501754cafe861cdb30d70b06a211077
Jay Kan <leicasper@gmail.com>
2017-02-14 12:37:14 -0800

    feat(photo): add photo cmp

---
+++ src/filters/components/photo/photo.ts
@@ -0,0 +1,22 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FilterStyle, OverlayStyle } from 'src/filters';
+import './photo.scss';
+
+@Component({
+  selector: 'photo',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template:`
+  <div class="photo">
+    <figure [ngStyle]="filter">
+      <div [ngStyle]="overlay"></div>
+      <image-loader [image]="image"></image-loader>
+    </figure>
+  </div>
+  `
+})
+export class PhotoComponent {
+  @Input() overlay: OverlayStyle;
+  @Input() filter: FilterStyle;
+  @Input() image: string;
+}
