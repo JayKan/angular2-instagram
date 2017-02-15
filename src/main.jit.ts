4712deccdf2a6a873316de7bd74235fb5786b74c
Jay Kan <leicasper@gmail.com>
2017-02-15 14:37:15 -0800

    chore: add disableDebugTools() for JIT mode

--- src/main.jit.ts
+++ src/main.jit.ts
@@ -1,8 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { disableDebugTools } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 
 // enable prod for faster renders
 if (process.env.NODE_ENV === 'production') {
+  disableDebugTools();
   enableProdMode();
 }
 
@@ -13,7 +15,7 @@ import './shared/styles/styles.scss';
 import { AppModule } from './app';
 
 export function main() {
-  return platformBrowserDynamic()
+  platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch(err => console.error(err));
 }
