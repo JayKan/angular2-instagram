3ad3ec0882aa5bc9bea05761b732b08de38b74a1
Jay Kan <leicasper@gmail.com>
2017-02-16 09:19:05 -0800

    chore: expose filters core module

---
+++ src/filters/core/index.ts
@@ -0,0 +1,4 @@
+export { MdGestureConfig } from './gestures';
+export { applyCssTransform } from './style';
+export { coerceBooleanProperty } from './coercion';
+export { UniqueSelectionDispatcher } from './coordination';
