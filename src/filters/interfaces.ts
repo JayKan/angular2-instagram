c85883931b5e781a18ed0a603a25fa7a22307bec
Jay Kan <leicasper@gmail.com>
2017-02-16 09:19:47 -0800

    chore: add filters related interfaces

---
+++ src/filters/interfaces.ts
@@ -0,0 +1,31 @@
+export interface FilterStyle {
+  WebkitFilter: string;
+  filter: string;
+  position: string;
+}
+
+export interface OverlayStyle {
+  content: string;
+  display: string;
+  height: string;
+  width: string;
+  top: string;
+  left: string;
+  pointerEvents: string;
+  position: string;
+  mixBlendMode?: string;
+  opacity: number;
+  background: string;
+}
+
+export interface GalleryModel {
+  figureStyle: FilterStyle;
+  overlayStyle: OverlayStyle;
+  image: string;
+  labelName: string;
+}
+
+export interface HammerInstance {
+  on(eventName: string, callback: Function): void;
+  off(eventName: string, callback: Function): void;
+}
